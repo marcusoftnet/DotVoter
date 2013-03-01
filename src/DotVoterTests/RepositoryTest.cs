fd2d6c1c93cd12cb788cfa6044aef1ab4896e70b
carllindelof <carl.lindelof@gmail.com>
2013-03-01 20:00:49 +0100

    project structure and first lab

---
+++ src/DotVoterTests/RepositoryTest.cs
@@ -0,0 +1,19 @@
+﻿using DotVoter;
+using Xunit;
+
+namespace DotVoterTests
+{
+    public class RepositoryTest
+    {
+        [Fact]
+        public void MyTest()
+        {
+            var repo = new EventRepoTest();
+
+            repo.Test();
+
+
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
