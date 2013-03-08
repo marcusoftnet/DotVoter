1781d288543cdea04770d182f28044881f2c28da
carllindelof <carl.lindelof@gmail.com>
2013-03-08 22:34:14 +0100

    remove test used for integration

--- src/DotVoter/EventRepoTest.cs
+++ src/DotVoter/EventRepoTest.cs
@@ -10,70 +10,70 @@ using System.Linq;
 
 namespace DotVoter
 {
-    public class EventRepoTest : IDisposable
-    {
-        private readonly MongoRepository<WorkShopEvent> _repo;
+    //public class EventRepoTest : IDisposable
+    //{
+    //    private readonly MongoRepository<WorkShopEvent> _repo;
 
-        public EventRepoTest()
-        {
-            var container = ObjectFactory.Container;
+    //    public EventRepoTest()
+    //    {
+    //        var container = ObjectFactory.Container;
 
-            container.Configure(config => config.For<IIdentityGenerator>().Use<IdentityGenerator>());
+    //        container.Configure(config => config.For<IIdentityGenerator>().Use<IdentityGenerator>());
 
-            NancyBootstrapper.RegisterMongoMappings(container);
+    //        NancyBootstrapper.RegisterMongoMappings(container);
 
-            _repo = new MongoRepository<WorkShopEvent>();
-        }
+    //        _repo = new MongoRepository<WorkShopEvent>();
+    //    }
 
-        [Fact]
-        public void Test()
-        {
-            var userIdentfier = "Marcus";
-            // adding new entity
-            var newEvent = new WorkShopEvent()
-                {
-                    Name = "MyEvent",
-                    Description = "SuperduperEvent",
-                    Topics = new List<Topic>()
-                };
-
-
-            // adding a Topic 
-            var newTopic = new Topic
-                {
-                    Id = 16,
-                    Name = "Ny lapp",
-                    Votes = new List<Vote>() {new Vote() {UserIdentfier = userIdentfier}}
-                };
+    //    [Fact]
+    //    public void Test()
+    //    {
+    //        var userIdentfier = "Marcus";
+    //        // adding new entity
+    //        var newEvent = new WorkShopEvent()
+    //            {
+    //                Name = "MyEvent",
+    //                Description = "SuperduperEvent",
+    //                Topics = new List<Topic>()
+    //            };
 
 
-            newEvent.Topics.Add(newTopic);
+    //        // adding a Topic 
+    //        var newTopic = new Topic
+    //            {
+    //                Id = 16,
+    //                Name = "Ny lapp",
+    //                Votes = new List<Vote>() {new Vote() {UserIdentfier = userIdentfier}}
+    //            };
 
-            _repo.Add(newEvent);
 
-            // searching
-            var result = _repo.All(c => c.Name == "MyEvent").First();
+    //        newEvent.Topics.Add(newTopic);
 
-            // updating 
-            newEvent.Description = "Just as good one!";
-            _repo.Update(newEvent);
-            RemoveVoteFromTopic(result.Id, result.Topics.First().Id, userIdentfier);
-        }
+    //        _repo.Add(newEvent);
 
-        protected void RemoveVoteFromTopic(int eventId, int topicId, string uniqueIdentifier)
-        {
-            var vote = new Vote {UserIdentfier = uniqueIdentifier};
+    //        // searching
+    //        var result = _repo.All(c => c.Name == "MyEvent").First();
 
-            var query = Query.And(Query.EQ("_id", eventId), Query.EQ("Topics._id", topicId));
+    //        // updating 
+    //        newEvent.Description = "Just as good one!";
+    //        _repo.Update(newEvent);
+    //        RemoveVoteFromTopic(result.Id, result.Topics.First().Id, userIdentfier);
+    //    }
 
-            var update = Update.PullWrapped("Topics.$.Votes", vote.ToBsonDocument());
+    //    protected void RemoveVoteFromTopic(int eventId, int topicId, string uniqueIdentifier)
+    //    {
+    //        var vote = new Vote {UserIdentfier = uniqueIdentifier};
 
-            _repo.Collection.Update(query, update);
-        }
+    //        var query = Query.And(Query.EQ("_id", eventId), Query.EQ("Topics._id", topicId));
 
-        public void Dispose()
-        {
-            Console.WriteLine("We are currently disposing.");
-        }
-    }
+    //        var update = Update.PullWrapped("Topics.$.Votes", vote.ToBsonDocument());
+
+    //        _repo.Collection.Update(query, update);
+    //    }
+
+    //    public void Dispose()
+    //    {
+    //        Console.WriteLine("We are currently disposing.");
+    //    }
+    //}
 }
